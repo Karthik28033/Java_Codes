25c8578460d43d12ea77d7fe3081da1f833dc081
Sundarraj9641 <111488540+Sundarraj9641@users.noreply.github.com>
2023-09-02 20:56:56 +0530

    Values Updated

--- src/com/programiz/Add_To_Integer.java
+++ src/com/programiz/Add_To_Integer.java
@@ -2,8 +2,8 @@ package com.programiz;
 //Java Program to Add Two Integers
 public class Add_To_Integer {
 public static void main(String[]args) {
-int first_number=10;
-int second_number=20;
+int first_number=200;
+int second_number=60;
 int sum=first_number+second_number;
 System.out.println("the sum is"+" "+sum);
 }
